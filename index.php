c894e329795cf5d2c85a07f7a1bce7bc16b2b16e
yogita <yogitapaliwal99@gmail.com>
2021-01-22 22:41:33 +0530

    my project

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+
+   <head>
+          <?php include "head.html";?>
+    </head>
+   <body>
+   
+             <?php include "index.html";?>
+			 <?php include "card.html";?>
+			 
+			 
+   </body>
+   </html>
